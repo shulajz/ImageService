27479faffa4cb25194fcaba69f0cf71184556e93
or shaashua <orshaashua@gmail.com>
2018-05-01 21:08:31 +0300

    change

--- ImageServiceGUI/MainWindow.xaml.cs
+++ ImageServiceGUI/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ImageServiceGUI
-{
+{ //
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
