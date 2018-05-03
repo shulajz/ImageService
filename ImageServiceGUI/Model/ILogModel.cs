d727d9875b27c84faaa467caf06a1182122d61b5
or shaashua <orshaashua@gmail.com>
2018-05-03 13:19:44 +0300

    xaml log

--- ImageServiceGUI/Model/ILogModel.cs
+++ ImageServiceGUI/Model/ILogModel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+
 
 namespace ImageServiceGUI.Model
 {
-    class ILogModel
+    interface ILogModel
     {
+        event PropertyChangedEventHandler PropertyChanged;
+
+        //event PropertyChangedEventHandler PropertyChanged;
+        string Type { get; set; }
+        string Message { get; set; }
     }
 }
--- ImageServiceGUI/Model/LogModel.cs
+++ ImageServiceGUI/Model/LogModel.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace ImageServiceGUI.Model
 {
-    class LogModel: ILogModel
+    class LogModel : ILogModel, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private string m_type;
+        public string Type
+        {
+            get { return m_type; }
+            set
+            {
+                m_type = value;
+                OnPropertyChanged("type");
+            }
+        }
+
+        private string m_message;
+        public string Message
+        {
+            get { return m_message; }
+            set
+            {
+                m_message = value;
+                OnPropertyChanged("message");
+            }
+        }
+
+
     }
 }
