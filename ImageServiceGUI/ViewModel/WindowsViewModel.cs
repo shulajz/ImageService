0dbfa18011c9d2936ed597e80a1bb7d72fcb6894
Shulamit Ajzner <shulajz@hotmail.com>
2018-05-17 14:18:26 +0300

    on close command

--- ImageServiceGUI/ViewModel/WindowsViewModel.cs
+++ ImageServiceGUI/ViewModel/WindowsViewModel.cs
@@ -1,20 +1,30 @@
 ﻿using ImageServiceGUI.Model;
+using Microsoft.Practices.Prism.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ImageServiceGUI.ViewModel
 {
     class WindowsViewModel
     {
         private IWindowsModel m_windowsModel;
+        public ICommand WindowClosing { get; private set; }
+
         public WindowsViewModel(IWindowsModel windowsModel)
         {
+            this.WindowClosing = new DelegateCommand<object>(this.OnWindowClosing);
             m_windowsModel = windowsModel;
         }
-       
+
+        private void OnWindowClosing(object obj)
+        {
+            //put code here
+        }
+
         public string BackgroundColor
         {
             get { return m_windowsModel.BackgroundColor; }
