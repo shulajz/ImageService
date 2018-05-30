1badfe967133520dc701d2b3acda367739f59339
user <user@USERPC>
2018-05-30 15:27:44 +0300

    config

--- WebApplication1/Models/FirstModel.cs
+++ WebApplication1/Models/FirstModel.cs
@@ -2,27 +2,49 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace WebApplication1.Models
+namespace ImageServiceWeb.Models
 {
     public class FirstModel
     {
-
+        private Setting m_setting;
         FirstModel()
         {
             ObservableCollection<string> handlers = new ObservableCollection<string>();
             handlers.Add("or");
             handlers.Add("shula");
-            Setting setting = new Setting()
+            m_setting = new Setting()
             {
                 OutPutDir = "output dir",
                 SourceName = "source name",
                 LogName = "log name",
                 ThumbnailSize = 120,
-                ArrHandlers = null
+                ArrHandlers = handlers
             };
-        
+            OutPutDir = m_setting.OutPutDir;
         }
+      
+        [Required]
+        [DataType(DataType.Text)]
+        //[Display(Name = "OutPutDir")]
+        public string OutPutDir { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "SourceName")]
+        public string SourceName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "LogName")]
+        public string LogName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "ThumbnailSize")]
+        public int ThumbnailSize { get; set; }
     }
+}
--- WebApplication1/Properties/AssemblyInfo.cs
+++ WebApplication1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApplication1")]
+[assembly: AssemblyTitle("ImageServiceWeb")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplication1")]
+[assembly: AssemblyProduct("ImageServiceWeb")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
