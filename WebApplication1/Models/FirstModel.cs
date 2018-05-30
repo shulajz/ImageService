d3899d2dfc3f9d9330b6041d37a71b51241a8d74
Shulamit Ajzner <shulajz@hotmail.com>
2018-05-30 11:49:26 +0300

    settings

--- WebApplication1/Models/FirstModel.cs
+++ WebApplication1/Models/FirstModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ImageService.Communication.Modal;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,20 @@ namespace WebApplication1.Models
 {
     public class FirstModel
     {
+
+        FirstModel()
+        {
+            ObservableCollection<string> handlers = new ObservableCollection<string>();
+            handlers.Add("or");
+            handlers.Add("shula");
+            Setting setting = new Setting()
+            {
+                OutPutDir = "output dir",
+                SourceName = "source name",
+                LogName = "log name",
+                ThumbnailSize = 120,
+                ArrHandlers = null
+            };
+        
+        }
     }
-}
